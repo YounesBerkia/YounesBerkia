92b79b70a60b368381bd1dddeb1ce3d64fc45517
Younes <70062970+YounesBerkia@users.noreply.github.com>
2021-07-08 14:26:35 +0200

    Update FolderSorter.py

--- FolderSorter.py
+++ FolderSorter.py
@@ -2,14 +2,19 @@ import os
 import time
 import shutil
 
-dir =      input('Enter Directory >> ')
-music =     'C:\\Users\\berki\\Music'
-school =     'C:\\Users\\berki\\Schule'
-python_code = 'C:\\Users\\berki\\Python'
-wallpapers =   'C:\\Users\\berki\\Desktop\\Main\\Wallpapers'
-downloads =     'C:\\Users\\berki\\Downloads'
-programs =       'C:\\Users\\berki\\Desktop\\Main\\Programme'
-unknown =         'C:\\Users\\berki\\Desktop\\Main\\Unknown'
+
+dir =                      input('Enter Directory >> ')
+print()
+
+
+#Input your own directorys
+music =               'C:\\Users\\berki\\Desktop\\Main\\Music\\'
+school =        'C:\\Users\\berki\\Desktop\\Main\\Schule\\'
+python_code = 'C:\\Users\\berki\\Desktop\\Main\\Python\\'
+wallpapers =   'C:\\Users\\berki\\Desktop\\Main\\Wallpapers\\'
+downloads =         'C:\\Users\\berki\\Downloads\\'
+programs =              'C:\\Users\\berki\\Desktop\\Main\\Programme\\'
+unknown =                   'C:\\Users\\berki\\Desktop\\Main\\Unknown\\'
 
 
 if dir == 'music':
@@ -30,23 +35,57 @@ if dir == 'downloads':
 if dir == 'programs' or dir == 'programme':
     dir = programs
 
-for file in os.listdir(dir):
+running = True
 
-    if file.endswith(".mp3") or file.endswith(".wav"):
-        shutil.move(f"{dir}/{file}", music)
+while running == True:
+    for file in os.listdir(dir):
+        try:
 
+            if file.endswith(".mp3") or file.endswith(".wav"):
+                shutil.move(f"{dir}/{file}", music)
+                print('[+] Found music file!')
+                print('-    ', file)
+                print('\n')
 
-    if file.endswith(".txt") or file.endswith(".pdf") or file.endswith(".docx"):
-        shutil.move(f"{dir}/{file}", school)
+                if file.endswith(".txt") or file.endswith(".pdf") or file.endswith(".docx"):
+                    shutil.move(f"{dir}/{file}", school)
+                    print('[+] Found file for school!')
+                    print('-    ', file)
+                    print('\n')
 
-    if file.endswith(".py"):
-        shutil.move(f"{dir}/{file}", python_code)
-    
-    if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
-        shutil.move(f"{dir}/{file}", wallpapers)
+            if file.endswith(".py"):
+                shutil.move(f"{dir}/{file}", python_code)
+                print('[+] Found python file!')
+                print('-    ', file)
+                print('\n')
 
-    if file.endswith(".exe"):
-        shutil.move(f"{dir}/{file}", programs)
+            if file.endswith(".jpg") or file.endswith(".png") or file.endswith(".jpeg"):
+                shutil.move(f"{dir}/{file}", wallpapers)
+                print('[+] Found image!')
+                print('-    ', file)
+                print('\n')
 
-    else:
-        shutil.move(f"{dir}/{file}", unknown)
+            if file.endswith(".exe"):
+                shutil.move(f"{dir}/{file}", programs)
+                print('[+] Found exe file!')
+                print('-    ', file)
+                print('\n')
+
+            else:
+                shutil.move(f"{dir}/{file}", unknown)
+                print('[+] Found unknown file!')
+                print('-    ', file)
+                print('\n')
+
+                print('[+] Finished!')
+                running = False
+
+        except:
+            print('[+] Finished!')
+            running = False
+            pass
+
+running = False
+
+time.sleep(5)
+os.close()
